5d4a97923fd6bfb7dc0a75578af0548a56166d10
Giovani Carnaval <gi.carnaval@hotmail.com>
2025-11-29 19:58:16 -0300

    tesete

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,3 +1,3 @@
 export const coupleName = import.meta.env.VITE_COUPLE_NAME;
 export const driveLink = import.meta.env.VITE_DRIVE_LINK;
-export const defaultMessage = import.meta.env.VITE_DEFAULT_MESSAGE;
+export const defaultMessage = `Olá! Aqui estão as fotos do casamento de ${coupleName}: ${driveLink}`
