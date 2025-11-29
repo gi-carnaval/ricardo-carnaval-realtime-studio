331511180112623b49a261365ab3e40547fe99df
Giovani Carnaval <gi.carnaval@hotmail.com>
2025-11-29 17:42:09 -0300

    feat: add env variables

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,3 +1,3 @@
-export const coupleName = "Larissa & "
-export const driveLink = "https://drive.google.com/drive/folders/1WRkfCW4Abnb5yMlah6L7KqQgC7Yi6Z1z"
-export const defaultMessage = `Olá! Aqui estão as fotos do casamento de ${coupleName}: ${driveLink}`
+export const coupleName = import.meta.env.VITE_COUPLE_NAME;
+export const driveLink = import.meta.env.VITE_DRIVE_LINK;
+export const defaultMessage = import.meta.env.VITE_DEFAULT_MESSAGE;
